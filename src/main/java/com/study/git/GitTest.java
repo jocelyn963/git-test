fdc69d40bcf10d72c55c176291723e8ad6a86ceb
jocelyn <1608291894@qq.com>
2022-07-24 08:55:14 +0800

    first commit

---
+++ src/main/java/com/study/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.study.git;
+
+/**
+ * @author jyr
+ * @create 2022-07-24-8:45
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}

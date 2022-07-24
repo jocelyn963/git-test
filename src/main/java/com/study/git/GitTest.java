e39096d1e62c002702e489e660944e53140ef8c4
jocelyn <1608291894@qq.com>
2022-07-24 09:47:19 +0800

    master commit 3

--- src/main/java/com/study/git/GitTest.java
+++ src/main/java/com/study/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello4");
         System.out.println("master test2");
         System.out.println("hot-fix test2");
+        System.out.println("master test3");
     }
 }
